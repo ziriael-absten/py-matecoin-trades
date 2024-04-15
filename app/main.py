b0d827226918dcfaab8f3051a9a298367b648ccf
Ziriael Absten <innli1322@gmail.com>
2024-04-15 14:45:11 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,23 +4,19 @@ from decimal import Decimal
 
 
 def calculate_profit(name: str) -> None:
-    with open(name, "r") as trades, open("profit.json", "w") as profit:
+    with (open(name, "r") as trades, open("profit.json", "w") as profit):
         trades_data = json.load(trades)
         earned_money = 0
         matecoin_account = 0
         for trade in trades_data:
             if trade["bought"]:
                 matecoin_account += Decimal(trade["bought"])
-                earned_money -= (
-                        Decimal(trade["bought"])
-                        * Decimal(trade["matecoin_price"])
-                )
+                earned_money -= Decimal(trade["bought"]) * \
+                    Decimal(trade["matecoin_price"])
             if trade["sold"]:
                 matecoin_account -= Decimal(trade["sold"])
-                earned_money += (
-                        Decimal(trade["sold"])
-                        * Decimal(trade["matecoin_price"])
-                )
+                earned_money += Decimal(trade["sold"]) * \
+                    Decimal(trade["matecoin_price"])
         result = {
             "earned_money": str(earned_money),
             "matecoin_account": str(matecoin_account)
